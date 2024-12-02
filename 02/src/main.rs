e7f63db2838a9e60b821893b67376c6d5bc2c34a
Peter Nirschl <peter.nirschl@gmail.com>
2024-12-02 16:07:55 +0100

    day 02 / part 2
    Signed-off-by: Peter Nirschl <peter.nirschl@gmail.com>

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
 
+use itertools::Itertools;
+
 fn main() {
     let mut line = String::new();
     let mut save_reports: u64 = 0;
+    let mut save_reports2: u64 = 0;
 
     // read and parse input
     let stdin = io::stdin();
@@ -18,6 +21,7 @@ fn main() {
             .map(|s| s.parse::<i64>().unwrap())
             .collect();
 
+        // part 1
         let ascending = levels.windows(2).all(|w| w[0] < w[1]);
         let descending = levels.windows(2).all(|w| w[0] > w[1]);
         let correct_diff = levels.windows(2).all(|w| {
@@ -29,8 +33,25 @@ fn main() {
             save_reports += 1;
         }
 
+        // part 2
+        let dampened_save = levels.iter().combinations(levels.len() - 1).any(|subset| {
+            let ascending = subset.windows(2).all(|w| w[0] < w[1]);
+            let descending = subset.windows(2).all(|w| w[0] > w[1]);
+            let correct_diff = subset.windows(2).all(|w| {
+                let delta = (w[0] - w[1]).abs();
+                delta >= 1 && delta <= 3
+            });
+
+            correct_diff && (ascending || descending)
+        });
+
+        if dampened_save {
+            save_reports2 += 1;
+        }
+
         line.clear();
     }
 
     println!("{save_reports}");
+    println!("{save_reports2}");
 }
